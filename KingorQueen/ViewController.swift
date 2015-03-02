72a299b9950d94c2f6ed88a70abcee260885c510
Tris Earl <Tristan@Triss-MacBook-Pro.local>
2015-03-02 15:56:13 +0000

    Initial Commit

---
+++ KingorQueen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KingorQueen
+//
+//  Created by Tris Earl on 02/03/2015.
+//  Copyright (c) 2015 Tristan Earl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
