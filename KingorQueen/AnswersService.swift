1a8b8a184c6d4cfc4b385c3090d16c8b9cc3b093
Joel Trew <joel@trew.org.uk>
2015-05-01 11:17:45 +0100

    Implements JSON functionality so questions and answers can be changed remotely

--- KingorQueen/AnswersService.swift
+++ KingorQueen/AnswersService.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 protocol AnswersServiceDelegate: class {
-    func didLoadAnswers(answers: [Question])
+    func didLoadQuestionsAndAnswers(questionsAndAnswers: [Question])
 }
 
 
@@ -19,50 +19,56 @@ class AnswersService: NSObject {
     
     weak var delegate: AnswersServiceDelegate?
     private let answersDataPath: String
+    
     init(answersDataPath: String) {
         self.answersDataPath = answersDataPath
     }
     
     
     func loadAnswers() {
-        let url = NSURL(string:"http://joeltrew.com/kfadAnswers.json")!
+        //load json url
+        let url = NSURL(string:answersDataPath)!
         let request = NSURLRequest(URL: url)
         let data = NSData(contentsOfURL: url)
+        //init json with url data
         let json:JSON = JSON(data:data!)
 
         var questionsArray = [Question]()
+        //set up questionsArray which will hold our quetions which will in turn hold our answers
         
         for (key: String, que: JSON) in json["questions"] {
+            //loop through all the questions
             
             var answersArray = [Answer]()
-            
-            
-            
+            //create an Array to store our Answers in
             
             for(index:String, ans:JSON) in que["answers"] {
-                
-                //println(ans["answerText"])
+                //loop through all the answers in a question
                 
                 var answertext = ans["answerText"].string
                 var answerstrength = ans["answerStrength"].int
                 var answermorality = ans["answerMorality"].int
                 var answerhumour = ans["answerHumour"].int
                 var answermodern = ans["answerModern"].int
+                //set json data values to new variables we will use
                 
                 var answers = Answer(answerText: answertext!, answerStrength: answerstrength!, answerMorality: answermorality!, answerHumour: answerhumour!, answerModern: answermodern!)
+                //initialise Answer object with new variables from the json
                 answersArray.append(answers)
-                
-            
+                //add/append that Answer to the array we made
             }
+            
             var questiontext = que["question"].string
+            //get the question text variable from json data
+            
             var question = Question(questionText: questiontext!, answers: answersArray)
+            //initalise a new Question object with both the question text and the array of Answers we just made
             
             questionsArray.append(question)
-      
+            //add the Question(with child answers) to large questionArray which we will pass to the viewcontroller
             
         }
-        self.delegate?.didLoadAnswers(questionsArray)
-        println(questionsArray.count)
-        println(questionsArray[0].answers[1].answerText)
+        self.delegate?.didLoadQuestionsAndAnswers(questionsArray)
+        //delegate the questionsArray which is full of our questions and answers
     }
 }
--- KingorQueen/ViewController.swift
+++ KingorQueen/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  King for a Day
-//
-//  Created by Tristan Earl on 02/03/2015.
-//  Copyright (c) 2015 Tristan Earl. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -20,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button4: UIButton!
     var buttonArray: [UIButton] {return [button1, button2, button3, button4 ]}
     
-   //question variables
+    //question variables
     var questions = [Question]()
     var currentQuestionIndex = 0
     var currentQuestion: Question { return questions[currentQuestionIndex] }
@@ -30,7 +22,7 @@ class ViewController: UIViewController {
     var player = Player(name: "Dan", gender: .Male)
     //
     
-   //json variables
+    //json variables
     let answersDataPath = "http://joeltrew.com/kfadAnswers.json"
     var answersService: AnswersService!
     //
@@ -39,54 +31,11 @@ class ViewController: UIViewController {
         super.init(coder: aDecoder)
         
         
-        
-        //Initialises Answers for Question 1
-        var question1Answers = [
-            Answer(answerText: "Declare War", answerStrength: 20, answerMorality: -10, answerHumour: 0, answerModern: 0),
-            Answer (answerText: "Burn the Leaders at the Stake", answerStrength: 5, answerMorality: -25, answerHumour: 5, answerModern: -25),
-            Answer(answerText: "Post a Facebook Status", answerStrength: -20, answerMorality: 0, answerHumour: 25, answerModern: 25),
-            Answer(answerText: "Give them what they want", answerStrength: -15, answerMorality: 25, answerHumour: 0, answerModern: 0)
-        ]
-        
-        // Initialises Question 1
-        var question1 = Question(questionText: "The Peasants are Rebelling. \n What do you do?", answers: question1Answers)
-        
-        //Adds Question 1 to question array
-        questions.append(question1)
-        
-        //Initialises Answers for Question 2
-        var question2Answers = [
-            Answer(answerText: "Raise Taxes", answerStrength: 10, answerMorality: -20, answerHumour: -10, answerModern: 0),
-            Answer(answerText: "Do Nothing", answerStrength: -20, answerMorality: -10, answerHumour: 10, answerModern: 10),
-            Answer(answerText: "Blame the Celts", answerStrength: -25, answerMorality: -5, answerHumour: 25, answerModern: -25),
-            Answer(answerText: "Steal from the Rich", answerStrength: 20, answerMorality: 5, answerHumour: 5, answerModern: 10)
-        ]
-        
-        //Initialises Question 2
-        var question2 = Question(questionText: "You've run out of Money. \n What do you do?", answers: question2Answers)
-        //Adds Question 2 to question array
-        questions.append(question2)
-        
-        
-        //Initialises Answers for Question 3
-        var question3Answers = [
-            Answer(answerText: "Raise 3xes", answerStrength: 10, answerMorality: -20, answerHumour: -10, answerModern: 0),
-            Answer(answerText: "Do Nothing", answerStrength: -20, answerMorality: -10, answerHumour: 10, answerModern: 10),
-            Answer(answerText: "Blame the Celts", answerStrength: -25, answerMorality: -5, answerHumour: 25, answerModern: -25),
-            Answer(answerText: "Steal from the Rich", answerStrength: 20, answerMorality: 5, answerHumour: 5, answerModern: 10)
-        ]
-        
-        //Initialises Question 3
-        var question3 = Question(questionText: "Test 3", answers: question3Answers)
-        //Adds Question 3 to question array
-        questions.append(question3)
-        
-        
     }
     
     
     func addScoreToPlayer(buttonIndex:Int) {
-        //adds answer scores to player scores and prints  
+        //adds answer scores to player scores and prints
         
         let answer = currentQuestion.answers[buttonIndex]
         player.strength += currentQuestion.answers[buttonIndex].answerStrength
@@ -96,13 +45,13 @@ class ViewController: UIViewController {
         player.printPlayerScores()
     }
     
-
-
+    
+    
     
     func finalScorePrint() {
-            
-            println("\n----FINAL SCORES----\n")
-            player.printPlayerScores()
+        
+        println("\n----FINAL SCORES----\n")
+        player.printPlayerScores()
     }
     
     
@@ -110,7 +59,7 @@ class ViewController: UIViewController {
         
         let lastQuestion = questions.count-1
         let buttonIndex = find(buttonArray, sender)!
-
+        
         
         if currentQuestionIndex == (lastQuestion){
             addScoreToPlayer(buttonIndex)
@@ -119,7 +68,7 @@ class ViewController: UIViewController {
             // do something else here which removes the buttons
         }else {
             
-
+            
             // Which button was tapped
             
             addScoreToPlayer(buttonIndex)
@@ -146,6 +95,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        answersService = AnswersService(answersDataPath: answersDataPath)
+        answersService.delegate = self
+        answersService.loadAnswers()
+        
         
         //sets question label to first question in question array
         questionlabel.text = questions[0].questionText
@@ -159,40 +112,27 @@ class ViewController: UIViewController {
             item.setTitle(currentQuestion.answers[index].answerText, forState: UIControlState.Normal)
             
             
-           
-            
         }
         
-      
-        answersService = AnswersService(answersDataPath: answersDataPath)
-        answersService.delegate = self
-        answersService.loadAnswers()
         
-    
-        
-    
     }
-    
-
-
     
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-
+        
     }
-    
-    
-    
     
     
 }
 
 extension ViewController: AnswersServiceDelegate {
-    func didLoadAnswers(answers:[Question]){
-        var questions = answers
+    func didLoadQuestionsAndAnswers(questionsAndAnswers:[Question]){
+        
+        questions = questionsAndAnswers
+        //set the array of json downloaded questions to the empty 'questions' array which our app will use
         
     }
 }
